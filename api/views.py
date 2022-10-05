cb8ffc91552d79f8cb5aa6736e9c3ae759a17712
Sachin Desai <sachin.desai1399@gmail.com>
2022-10-05 23:29:44 +0530

    Add Signup and Meter APIs

--- api/views.py
+++ api/views.py
@@ -19,13 +19,85 @@ def login_view(request):
         password = request.data['password']
         username_exists = User.objects.filter(Q(username=username) | Q(email=username)).last()
         data = {
+            'flash': False,
             'message': 'Invalid Username',
-            'username': None,
-            'user_id': None
+            'data': {
+                'username': '',
+                'user_id': ''
+            }
         }
         if username_exists:
             user = authenticate(request, username=username_exists.username, password=password)
+            data['flash'] = True if user else False
             data['message'] = 'Successful' if user else 'Unsuccessful'
-            data['username'] = user.username if user else None
-            data['user_id'] = user.id if user else None
+            data['data']['username'] = user.username if user else None
+            data['data']['user_id'] = user.id if user else None
         return Response(data)
+
+
+@api_view(['POST'])
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+        email = request.data['email']
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        contact_no = request.data['contact_no']
+        user = User.objects.filter(Q(username=username) | Q(email=email)).last()
+        data = {
+            'flash': False,
+            'message': 'Username/Email exists',
+            'data': {}
+        }
+        if not user:
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+                first_name=first_name,
+                last_name=last_name
+            )
+            user_data = UserInfo(
+                user=user,
+                contact=contact_no
+            )
+            user_data.save()
+            data['flash'] = True
+            data['message'] = 'Successful'
+        return Response(data)
+
+
+@api_view(['POST'])
+def meters_view(request):
+    if request.method == 'POST':
+        meter_name = request.data['meter_name']
+        poles_r = request.data['poles_r']
+        poles_y = request.data['poles_y']
+        poles_b = request.data['poles_b']
+        group = request.data['group']
+        period_type = request.data['period_type']
+        lat = request.data['lat']
+        lon = request.data['lon']
+        sunrise_offset = request.data['sunrise_offset']
+        sunset_offset = request.data['sunset_offset']
+        on_time = request.data['on_time']
+        off_time = request.data['off_time']
+        user_id = request.data['user_id']
+        meter = Meter(
+            meter_name = meter_name,
+            poles_r = poles_r,
+            poles_y = poles_y,
+            poles_b = poles_b,
+            group = group,
+            period_type = period_type,
+            lat = lat,
+            lon = lon,
+            sunrise_offset = sunrise_offset,
+            sunset_offset = sunset_offset,
+            on_time = on_time,
+            off_time = off_time,
+            created_by_id = user_id
+        )
+        meter.save()
+        return Response()
