e1a7185167ebc62935d2a9066b08ed60a17b924e
Sachin Desai <sachin.desai1399@gmail.com>
2022-10-09 16:49:44 +0530

    Configured CORS policies

--- api/views.py
+++ api/views.py
@@ -2,8 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from .models import *
+import json
 
 # Create your views here.
 @api_view(['POST'])
@@ -13,6 +15,7 @@ def user_view(request):
 
 
 @api_view(['POST'])
+@csrf_exempt
 def login_view(request):
     if request.method == 'POST':
         username = request.data['username']
@@ -100,4 +103,30 @@ def meters_view(request):
             created_by_id = user_id
         )
         meter.save()
+        parameters = json.loads(request.data['parameters'])
+        for parameter in parameters:
+            meter_parameter = MetersThreshold(
+                meter = meter,
+                parameter_name = parameter['parameter_name'],
+                field_name = parameter['field_name'],
+                unit = parameter['unit'],
+                min_thr = parameter['min_thr'],
+                max_thr = parameter['max_thr'],
+                notify = parameter['notify'],
+                created_by_id = user_id
+            )
+            meter_parameter.save()
         return Response()
+    elif request.method == 'GET':
+        user_id = request.query_params['user_id']
+        meters = list(Meter.objects.filter(created_by_id=user_id).values())
+        for meter in meters:
+            meter['parameters'] = list(MetersThreshold.objects.filter(meter_id=meter['id']).values())
+        data = {
+            'flash': True,
+            'message': 'Successful',
+            'data': {
+                'meters': meters
+            }
+        }  
+        return Response(data)
--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-js_vu_t4ifs!9hw!ts$t!+d6)^7()hea&4eg4#e0%%4@vfkg&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
 ]
@@ -46,9 +47,17 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://iotstreetlight.vercel.app",
 ]
 
 ROOT_URLCONF = 'backend.urls'
