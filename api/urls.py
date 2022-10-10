0b361103056dc86ad7ca34268304c8d1da5578d8
Sachin Desai <sachin.desai1399@gmail.com>
2022-10-10 22:08:19 +0530

    Fix urls.py error

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login_view, signup_view, meters_view
+from .views import login_view, signup_view, meters_view, meter_parameters_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('meters/', meters_view, name='meters'),
-    path('meter-parameters/', meters_parameters_view, name='meters-parameters'),
+    path('meter-parameters/', meter_parameters_view, name='meters-parameters'),
 ]
