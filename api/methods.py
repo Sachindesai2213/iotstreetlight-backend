32cedc8240f5a0f3df3db3518aa23c45af9e0eed
Sachin Desai <sachin.desai1399@gmail.com>
2022-11-03 13:49:48 +0530

    Fix Bug in meters API method

--- api/methods.py
+++ api/methods.py
@@ -3,7 +3,7 @@ from .models import *
 def meters_data(user_id):
     meters = list(Meter.objects.filter(created_by_id=user_id).values('id', 'meter_name', 'poles_r', 'poles_y', 'poles_b', 'group', 'period_type', 'lat', 'lon', 'sunrise_offset', 'sunset_offset', 'on_time', 'off_time', 'is_on'))
     for meter in meters:
-        meter['parameters'] = list(MetersThreshold.objects.filter(meter_id=meter['id']).values('id', 'meter_id', 'meter__meter_name]]]]]]]]]]]]]]]]]]]]]', 'parameter_name', 'field_name', 'unit', 'min_thr', 'max_thr', 'notify'))
+        meter['parameters'] = list(MetersThreshold.objects.filter(meter_id=meter['id']).values('id', 'meter_id', 'meter__meter_name', 'parameter_name', 'field_name', 'unit', 'min_thr', 'max_thr', 'notify'))
         for parameter in meter['parameters']:
             parameter['notify'] = 'Yes' if parameter['notify'] else 'No'
         meter['on_time'] = str(meter['on_time'])
