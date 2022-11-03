ac3819e341f2cb0e353bab0259f15670357b8ccd
Sachin Desai <sachin.desai1399@gmail.com>
2022-11-03 17:04:15 +0530

    Add MQTT client connection file

---
+++ backend/mqtt.py
@@ -0,0 +1,39 @@
+import django
+django.setup()
+
+from api.models import MeterData
+import paho.mqtt.client as mqtt
+import ast
+
+def on_connect(client,userdata, flags, rc):
+    print('Connected' + str(rc))
+    client.subscribe('sachin3913/feeds/python')
+
+def on_message(client, userdata, msg):
+    message_object = ast.literal_eval(msg.payload.decode())
+    meter_data = MeterData(
+        meter_id = message_object['meter_id'],
+        v_r = message_object['v_r'],
+        v_y = message_object['v_y'],
+        v_b = message_object['v_b'],
+        c_r = message_object['c_r'],
+        c_y = message_object['c_y'],
+        c_b = message_object['c_b'],
+        p_r = message_object['p_r'],
+        p_y = message_object['p_y'],
+        p_b = message_object['p_b'],
+        pf = message_object['pf'],
+        kvar = message_object['kvar'],
+        freq = message_object['freq'],
+        kwh = message_object['kwh'],
+        kvah = message_object['kvah'],
+    )
+    meter_data.save()
+    print('HI')
+
+client = mqtt.Client()
+client.username_pw_set(username='sachin3913', password='aio_ZBiF663ypMIa4GaCNiDeUWH7MMT2')
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("io.adafruit.com", 1883, 60)
