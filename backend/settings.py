60a4650fe5dc41978d2602c759e048139da99860
Sachin Desai <sachin.desai1399@gmail.com>
2022-11-03 17:16:21 +0530

    Add adafruti to allowed origins (2)

--- backend/settings.py
+++ backend/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://iotstreetlight.vercel.app",
+    "https://io.adafruit.com",
 ]
 
 ROOT_URLCONF = 'backend.urls'
